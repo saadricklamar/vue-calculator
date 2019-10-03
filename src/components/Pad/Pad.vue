3ca1c429b6bbd86af6ec29831eda5af82b4c2f73
Saad Baradan <saadbaradan@gmail.com>
2019-10-03 16:04:25 -0600

    Create pad component which will contain calculator buttons

---
+++ src/components/Pad/Pad.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="pad">
+    <article class="buttons">
+
+    </article>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Pad"
+};
+</script>
+
+<style scoped>
+.pad {
+  display: flex;
+  justify-content: center;
+}
+
+article {
+  width: 525px;
+  height: 400px;
+  background-color: #42b983;
+  border-radius: 5px;
+}
+</style>
