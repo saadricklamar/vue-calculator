f1f610fe56bde91b26572162219bfde3f7fc60f0
Saad Baradan <saadbaradan@gmail.com>
2019-10-09 14:18:07 -0600

    Test that when buttons 7, 8, and 9 are clicked, displayBtnValue is called

--- src/components/__tests__/SecondRowButtons.test.js
+++ src/components/__tests__/SecondRowButtons.test.js
@@ -13,4 +13,19 @@ describe("SecondRowButtons", () => {
   it("should have the props of displayBtnValue", () => {
     expect(wrapper.props("displayBtnValue")).toBeTruthy();
   });
+  it("should call displayBtnValue when 7 is clicked", () => {
+    wrapper.vm.displayBtnValue = jest.fn();
+    wrapper.find("button.seven").trigger("click");
+    expect(wrapper.vm.displayBtnValue).toHaveBeenCalled();
+  });
+  it("should call displayBtnValue when 8 is clicked", () => {
+    wrapper.vm.displayBtnValue = jest.fn();
+    wrapper.find("button.eight").trigger("click");
+    expect(wrapper.vm.displayBtnValue).toHaveBeenCalled();
+  });
+  it("should call displayBtnValue when 9 is clicked", () => {
+    wrapper.vm.displayBtnValue = jest.fn();
+    wrapper.find("button.nine").trigger("click");
+    expect(wrapper.vm.displayBtnValue).toHaveBeenCalled();
+  });
 });
