25846fb051678eb863072698b2414c9b95b22e00
Saad Baradan <saadbaradan@gmail.com>
2019-10-10 14:52:40 -0600

    Test that when AC button is clicked, counter is set to 0

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,9 +1,15 @@
 import { mount } from "@vue/test-utils";
 import App from "../../App";
 
+
 describe("App", () => {
   const wrapper = mount(App);
   it("should match the snapshot", () => {
     expect(wrapper.isVueInstance()).toMatchSnapshot();
+  });
+  it("should set the display to 0 when AC is clicked", () => {
+    wrapper.vm.clearDisplay = jest.fn();
+    wrapper.find("button.ac").trigger("click");
+    expect(wrapper.vm.counter).toBe(0);
   });
 });
