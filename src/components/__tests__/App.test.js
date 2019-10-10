c8ea5b04d9a9c62fcd76b384c48cba05703531ad
Saad Baradan <saadbaradan@gmail.com>
2019-10-10 15:52:25 -0600

    Test that when button numbers (1,2,3,etc.) are clicked the respective number is displayed

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -23,4 +23,49 @@ describe("App", () => {
     wrapper.find("button.percent").trigger("click");
     expect(wrapper.vm.counter).toBe(0.06);
   });
+  it("should display 1 when the button 1 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.one").trigger("click");
+    expect(wrapper.vm.counter).toBe("1");
+  });
+  it("should display 2 when the button 2 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.two").trigger("click");
+    expect(wrapper.vm.counter).toBe("2");
+  });
+  it("should display 3 when the button 3 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.three").trigger("click");
+    expect(wrapper.vm.counter).toBe("3");
+  });
+  it("should display 4 when the button 4 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.four").trigger("click");
+    expect(wrapper.vm.counter).toBe("4");
+  });
+  it("should display 5 when the button 5 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.five").trigger("click");
+    expect(wrapper.vm.counter).toBe("5");
+  });
+  it("should display 6 when the button 6 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.six").trigger("click");
+    expect(wrapper.vm.counter).toBe("6");
+  });
+  it("should display 7 when the button 7 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.seven").trigger("click");
+    expect(wrapper.vm.counter).toBe("7");
+  });
+  it("should display 8 when the button 8 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.eight").trigger("click");
+    expect(wrapper.vm.counter).toBe("8");
+  });
+  it("should display 9 when the button 9 is clicked", () => {
+    wrapper.vm.counter = 0;
+    wrapper.find("button.nine").trigger("click");
+    expect(wrapper.vm.counter).toBe("9");
+  });
 });
