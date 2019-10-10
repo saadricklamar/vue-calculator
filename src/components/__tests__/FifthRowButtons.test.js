de55ed0dbdf6d24f222fdcf09de27d8c762a5242
Saad Baradan <saadbaradan@gmail.com>
2019-10-10 11:42:01 -0600

    Test that the fifthrowbuttons component receives displayBtnValue and makeDecimal props

--- src/components/__tests__/FifthRowButtons.test.js
+++ src/components/__tests__/FifthRowButtons.test.js
@@ -2,8 +2,19 @@ import { mount } from "@vue/test-utils";
 import FifthRowButtons from "../FifthRowButtons/FifthRowButtons";
 
 describe("FifthRowButtons", () => {
-  const wrapper = mount(FifthRowButtons);
+  const wrapper = mount(FifthRowButtons, {
+    propsData: {
+      displayBtnValue: jest.fn(),
+      makeDecimal: jest.fn()
+    }
+  });
   it("should match the snapshot", () => {
     expect(wrapper.isVueInstance()).toMatchSnapshot();
+  });
+  it("should have the props of displayBtnValue", () => {
+    expect(wrapper.props("displayBtnValue")).toBeTruthy();
+  });
+  it("should have the props of makeDecimal", () => {
+    expect(wrapper.props("makeDecimal")).toBeTruthy();
   });
 });
