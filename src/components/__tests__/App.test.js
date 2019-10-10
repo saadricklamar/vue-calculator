430f00ae3ba7937d0713551fc565b582293f1a25
Saad Baradan <saadbaradan@gmail.com>
2019-10-10 15:42:10 -0600

    Test that when % is clicked 6 becomes 0.06, for example

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,15 +1,26 @@
 import { mount } from "@vue/test-utils";
 import App from "../../App";
 
-
 describe("App", () => {
-  const wrapper = mount(App);
+  const wrapper = mount(App, {
+    propsData: {
+      counter: 0
+    }
+  });
   it("should match the snapshot", () => {
     expect(wrapper.isVueInstance()).toMatchSnapshot();
   });
+  it("counter is zero by default", () => {
+    expect(wrapper.vm.counter).toBe(0);
+  });
   it("should set the display to 0 when AC is clicked", () => {
-    wrapper.vm.clearDisplay = jest.fn();
+    wrapper.vm.counter = 74;
     wrapper.find("button.ac").trigger("click");
     expect(wrapper.vm.counter).toBe(0);
+  });
+  it("should turn a number into a percent when % is clicked", () => {
+    wrapper.vm.counter = 6;
+    wrapper.find("button.percent").trigger("click");
+    expect(wrapper.vm.counter).toBe(0.06);
   });
 });
